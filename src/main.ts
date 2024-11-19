34e741ec59689b60bd13c78906fa2240a62b0aa2
Juan Yampara <yampara64@gmail.com>
2024-11-19 13:33:07 -0300

    crear-microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main-orders-Microservice');
+  
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      }
     }
   );
+
   await app.listen();
 }
 bootstrap();
--- src/modules/pedidos/pedidos.controller.ts
+++ src/modules/pedidos/pedidos.controller.ts
@@ -1,35 +1,36 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { FinalizarPedidoDto } from './dto/fin-pedido.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('pedidos')
 export class PedidosController {
     constructor(
         private readonly _pedidosService: PedidosService,
     ){}
-    @Post('generar')
-    public async generarPedido(@Body() pedido: CreatePedidoDto){
+    @MessagePattern({cmd: 'crear_pedido'})
+    public async generarPedido(@Payload() pedido: CreatePedidoDto){
         return await this._pedidosService.generatePedido(pedido);
     }
 
-    @Get()
+    @MessagePattern({cmd: 'find_all'})
     public async getAllPedidos(){
         return await this._pedidosService.getAllPedidos();
     }
 
-    @Get('estado/:id')
-    public async getEstadoPedido(@Param('id') id_pedido: string){
-        return await this._pedidosService.verEstado(+id_pedido);
+    @MessagePattern({cmd: 'get_estado'})
+    public async getEstadoPedido(@Payload('id', ParseIntPipe) id_pedido: number){
+        return await this._pedidosService.verEstado(id_pedido);
     }
     
-    @Get(':id')
-    public async getPedido(@Param('id') id_pedido: string){
-        return await this._pedidosService.getPedido(+id_pedido);
+    @MessagePattern({cmd: 'get_pedido'})
+    public async getPedido(@Payload('id', ParseIntPipe) id_pedido: number){
+        return await this._pedidosService.getPedido(id_pedido);
     }
 
-    @Patch('finalizar-pedido')
-    public async finalizarPedido(@Body() pedido: FinalizarPedidoDto){
+    @MessagePattern({cmd: 'finalizar_pedido'})
+    public async finalizarPedido(@Payload() pedido: FinalizarPedidoDto){
         return await this._pedidosService.finalizarPedido(pedido.id_pedido, pedido.fecha_fin)
     }
 }
